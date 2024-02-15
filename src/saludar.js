60bd429fb3970f0df69a4089afaa21c897e79431
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-15 10:00:06 -0400

    Se anade saludo por edad

--- src/saludar.js
+++ src/saludar.js
@@ -1,5 +1,11 @@
-function saludar(nombre) {
-  return nombre;
+function saludar(nombre,edad) {
+  let saludo = "Hola " + nombre;
+
+  if (edad > 30) {
+    saludo = "Hola Sr. " + nombre;
+  }
+
+  return saludo;
 }
 
 export default saludar;
