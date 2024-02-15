ff5ac83e1907d26c375930be73452e9ba93c347d
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-15 10:18:57 -0400

    Se anade funcionalidad de saludar por hora

--- src/saludar.js
+++ src/saludar.js
@@ -1,16 +1,27 @@
-function saludar(nombre,edad,genero) {
-  let saludo = "Hola " + nombre;
+function saludar(nombre, edad, genero) {
+  let saludo = "Hola " + obtenerSaludoSegunHora() + nombre;
 
   if (edad > 30) {
-    if (genero === "masculino"){
-      saludo = "Hola Sr. " + nombre;
-    }
-    else {
-      saludo = "Hola Sra. " + nombre;
+    if (genero === "masculino") {
+      saludo = "Hola Sr. " + obtenerSaludoSegunHora() + nombre;
+    } else {
+      saludo = "Hola Sra. " + obtenerSaludoSegunHora() + nombre;
     }
   }
 
   return saludo;
+}
+
+function obtenerSaludoSegunHora() {
+  const horaActual = new Date().getHours();
+
+  if (horaActual >= 5 && horaActual < 12) {
+    return "Buenos días, ";
+  } else if (horaActual >= 12 && horaActual < 18) {
+    return "Buenas tardes, ";
+  } else {
+    return "Buenas noches, ";
+  }
 }
 
 export default saludar;
