94597e631bcb4cc7bb694c2f09cb5205b949390e
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-20 08:16:00 -0400

    Se implemento eleccion de idioma EN o ES

--- src/presenter.js
+++ src/presenter.js
@@ -2,7 +2,8 @@ import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
-const genero_input = document.querySelector("#genero");
+const genero_select = document.querySelector("#genero");
+const idioma_select = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,7 +12,8 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
   const edad = parseInt(edad_input.value);
-  const genero = genero_input.value;
+  const genero = genero_select.value;
+  const idioma = idioma_select.value;
 
-  div.innerHTML = "<p>" + saludar(nombre, edad, genero) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, edad, genero, idioma) + "</p>";
 });
--- src/saludar.js
+++ src/saludar.js
@@ -1,26 +1,50 @@
-function saludar(nombre, edad, genero) {
-  let saludo = "Hola " + obtenerSaludoSegunHora() + nombre;
+function saludar(nombre, edad, genero, idioma) {
+  let saludo;
 
-  if (edad > 30) {
-    if (genero === "masculino") {
-      saludo = "Hola Sr. " + obtenerSaludoSegunHora() + nombre;
-    } else {
-      saludo = "Hola Sra. " + obtenerSaludoSegunHora() + nombre;
+  if (idioma === "es") {
+    saludo = obtenerSaludoSegunHora(idioma) + " Hola " + nombre;
+    
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludo = obtenerSaludoSegunHora() + "Sr. " + nombre;
+      } else {
+        saludo = obtenerSaludoSegunHora() + "Sra. " + nombre;
+      }
+    }
+  } else if (idioma === "en") {
+    saludo = obtenerSaludoSegunHora(idioma) + " Hello " + nombre;
+
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludo = obtenerSaludoSegunHora("en") + "Mr. " + nombre;
+      } else {
+        saludo = obtenerSaludoSegunHora("en") + "Mrs. " + nombre;
+      }
     }
   }
 
   return saludo;
 }
 
-function obtenerSaludoSegunHora() {
+function obtenerSaludoSegunHora(idioma) {
   const horaActual = new Date().getHours();
 
-  if (horaActual >= 5 && horaActual < 12) {
-    return "Buenos días, ";
-  } else if (horaActual >= 12 && horaActual < 18) {
-    return "Buenas tardes, ";
-  } else {
-    return "Buenas noches, ";
+  if (idioma === "es") {
+    if (horaActual >= 5 && horaActual < 12) {
+      return "Buenos días, ";
+    } else if (horaActual >= 12 && horaActual < 18) {
+      return "Buenas tardes, ";
+    } else {
+      return "Buenas noches, ";
+    }
+  } else if (idioma === "en") {
+    if (horaActual >= 5 && horaActual < 12) {
+      return "Good morning, ";
+    } else if (horaActual >= 12 && horaActual < 18) {
+      return "Good afternoon, ";
+    } else {
+      return "Good evening, ";
+    }
   }
 }
 
