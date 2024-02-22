da8522dd74124b47c1f2eecff517eb2c70cde7c1
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-22 10:22:46 -0400

    deberia generar Buzz para un numero que sigue la regla como el 10

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,10 @@
 function generarFizzBuzz(n){
     const FIZZ = 3;
+    const BUZZ = 5;
     if(n % FIZZ == 0){
         return "Fizz";
     }
-    if(n == 5){
+    if(n % BUZZ == 0){
         return "Buzz";
     }
     return n + "";
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -17,5 +17,8 @@ describe("FizzBuzz", () => {
     it("deberia generar Buzz para un numero que sigue la regla como el 5", () => {
         expect(generarFizzBuzz(5)).toEqual("Buzz");
     });
+    it("deberia generar Buzz para un numero que sigue la regla como el 10", () => {
+        expect(generarFizzBuzz(10)).toEqual("Buzz");
+    });
 
   });
